ad6ee5cf75d1f20b02d8b0d8be01ea11d93334d7
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-10-26 13:36:39 +0200

    Clean up conversion

--- test/device/wmma.jl
+++ test/device/wmma.jl
@@ -6,7 +6,7 @@
 
         @testset "wmma_store_d" begin
             buf     = Mem.alloc(Mem.Device, 16 * 16 * sizeof(Float32))
-            buf_ptr = convert(CUDAdrv.CuPtr{Float32}, buf)
+            buf_ptr = convert(CuPtr{Float32}, buf)
 
             function kernel(buf_ptr)
                 CUDAnative.wmma_store_d(buf_ptr)
@@ -16,7 +16,7 @@
             @cuda threads=32 kernel(buf_ptr)
 
             res = zeros(Float32, 16 * 16)
-            unsafe_copyto!(pointer(res), convert(CuPtr{Float32}, buf), 16 * 16)
+            unsafe_copyto!(pointer(res), buf_ptr, 16 * 16)
 
             @test all(res .== 42.0)
         end
