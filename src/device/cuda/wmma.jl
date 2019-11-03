910183910ec8c6fa76f345507da7be9f81d8c85a
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-11-03 18:56:37 +0100

    Adjust matrix multiplication to use NTuple

--- src/device/cuda/wmma.jl
+++ src/device/cuda/wmma.jl
@@ -107,30 +107,115 @@ for matrix in (:a, :b)
         convert(Int32, stride))
 end
 
-wmma_mma(a_0, a_1, a_2, a_3, a_4, a_5, a_6, a_7, b_0, b_1, b_2, b_3, b_4, b_5, b_6, b_7) =
+wmma_mma(a, b) =
     Base.llvmcall((
     "
     declare { float, float, float, float, float, float, float, float } @llvm.nvvm.wmma.mma.sync.col.col.m16n16k16.f32.f32(<2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, <2 x half>, float, float, float, float, float, float, float, float)
     ",
     "
-    %conv_0 = bitcast <2 x i16> %0 to <2 x half>
-    %conv_1 = bitcast <2 x i16> %1 to <2 x half>
-    %conv_2 = bitcast <2 x i16> %2 to <2 x half>
-    %conv_3 = bitcast <2 x i16> %3 to <2 x half>
-    %conv_4 = bitcast <2 x i16> %4 to <2 x half>
-    %conv_5 = bitcast <2 x i16> %5 to <2 x half>
-    %conv_6 = bitcast <2 x i16> %6 to <2 x half>
-    %conv_7 = bitcast <2 x i16> %7 to <2 x half>
-    %conv_8 = bitcast <2 x i16> %8 to <2 x half>
-    %conv_9 = bitcast <2 x i16> %9 to <2 x half>
-    %conv_10 = bitcast <2 x i16> %10 to <2 x half>
-    %conv_11 = bitcast <2 x i16> %11 to <2 x half>
-    %conv_12 = bitcast <2 x i16> %12 to <2 x half>
-    %conv_13 = bitcast <2 x i16> %13 to <2 x half>
-    %conv_14 = bitcast <2 x i16> %14 to <2 x half>
-    %conv_15 = bitcast <2 x i16> %15 to <2 x half>
+    %a_00 = extractvalue [16 x i16] %0, 0
+    %a_01 = extractvalue [16 x i16] %0, 1
+    %a_02 = extractvalue [16 x i16] %0, 2
+    %a_03 = extractvalue [16 x i16] %0, 3
+    %a_04 = extractvalue [16 x i16] %0, 4
+    %a_05 = extractvalue [16 x i16] %0, 5
+    %a_06 = extractvalue [16 x i16] %0, 6
+    %a_07 = extractvalue [16 x i16] %0, 7
+    %a_08 = extractvalue [16 x i16] %0, 8
+    %a_09 = extractvalue [16 x i16] %0, 9
+    %a_10 = extractvalue [16 x i16] %0, 10
+    %a_11 = extractvalue [16 x i16] %0, 11
+    %a_12 = extractvalue [16 x i16] %0, 12
+    %a_13 = extractvalue [16 x i16] %0, 13
+    %a_14 = extractvalue [16 x i16] %0, 14
+    %a_15 = extractvalue [16 x i16] %0, 15
 
-    %res = call { float, float, float, float, float, float, float, float } @llvm.nvvm.wmma.mma.sync.col.col.m16n16k16.f32.f32( <2 x half> %conv_0, <2 x half> %conv_1, <2 x half> %conv_2, <2 x half> %conv_3, <2 x half> %conv_4, <2 x half> %conv_5, <2 x half> %conv_6, <2 x half> %conv_7, <2 x half> %conv_8, <2 x half> %conv_9, <2 x half> %conv_10, <2 x half> %conv_11, <2 x half> %conv_12, <2 x half> %conv_13, <2 x half> %conv_14, <2 x half> %conv_15, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0)
+    %b_00 = extractvalue [16 x i16] %1, 0
+    %b_01 = extractvalue [16 x i16] %1, 1
+    %b_02 = extractvalue [16 x i16] %1, 2
+    %b_03 = extractvalue [16 x i16] %1, 3
+    %b_04 = extractvalue [16 x i16] %1, 4
+    %b_05 = extractvalue [16 x i16] %1, 5
+    %b_06 = extractvalue [16 x i16] %1, 6
+    %b_07 = extractvalue [16 x i16] %1, 7
+    %b_08 = extractvalue [16 x i16] %1, 8
+    %b_09 = extractvalue [16 x i16] %1, 9
+    %b_10 = extractvalue [16 x i16] %1, 10
+    %b_11 = extractvalue [16 x i16] %1, 11
+    %b_12 = extractvalue [16 x i16] %1, 12
+    %b_13 = extractvalue [16 x i16] %1, 13
+    %b_14 = extractvalue [16 x i16] %1, 14
+    %b_15 = extractvalue [16 x i16] %1, 15
+
+    %a_00_conv = bitcast i16 %a_00 to half
+    %a_01_conv = bitcast i16 %a_01 to half
+    %a_02_conv = bitcast i16 %a_02 to half
+    %a_03_conv = bitcast i16 %a_03 to half
+    %a_04_conv = bitcast i16 %a_04 to half
+    %a_05_conv = bitcast i16 %a_05 to half
+    %a_06_conv = bitcast i16 %a_06 to half
+    %a_07_conv = bitcast i16 %a_07 to half
+    %a_08_conv = bitcast i16 %a_08 to half
+    %a_09_conv = bitcast i16 %a_09 to half
+    %a_10_conv = bitcast i16 %a_10 to half
+    %a_11_conv = bitcast i16 %a_11 to half
+    %a_12_conv = bitcast i16 %a_12 to half
+    %a_13_conv = bitcast i16 %a_13 to half
+    %a_14_conv = bitcast i16 %a_14 to half
+    %a_15_conv = bitcast i16 %a_15 to half
+
+    %b_00_conv = bitcast i16 %b_00 to half
+    %b_01_conv = bitcast i16 %b_01 to half
+    %b_02_conv = bitcast i16 %b_02 to half
+    %b_03_conv = bitcast i16 %b_03 to half
+    %b_04_conv = bitcast i16 %b_04 to half
+    %b_05_conv = bitcast i16 %b_05 to half
+    %b_06_conv = bitcast i16 %b_06 to half
+    %b_07_conv = bitcast i16 %b_07 to half
+    %b_08_conv = bitcast i16 %b_08 to half
+    %b_09_conv = bitcast i16 %b_09 to half
+    %b_10_conv = bitcast i16 %b_10 to half
+    %b_11_conv = bitcast i16 %b_11 to half
+    %b_12_conv = bitcast i16 %b_12 to half
+    %b_13_conv = bitcast i16 %b_13 to half
+    %b_14_conv = bitcast i16 %b_14 to half
+    %b_15_conv = bitcast i16 %b_15 to half
+
+    %a_vec_00 = insertelement <2 x half> undef,     half %a_00_conv, i64 0
+    %a_vec_01 = insertelement <2 x half> %a_vec_00, half %a_01_conv, i64 1
+    %a_vec_02 = insertelement <2 x half> undef,     half %a_02_conv, i64 0
+    %a_vec_03 = insertelement <2 x half> %a_vec_02, half %a_03_conv, i64 1
+    %a_vec_04 = insertelement <2 x half> undef,     half %a_04_conv, i64 0
+    %a_vec_05 = insertelement <2 x half> %a_vec_04, half %a_05_conv, i64 1
+    %a_vec_06 = insertelement <2 x half> undef,     half %a_06_conv, i64 0
+    %a_vec_07 = insertelement <2 x half> %a_vec_06, half %a_07_conv, i64 1
+    %a_vec_08 = insertelement <2 x half> undef,     half %a_08_conv, i64 0
+    %a_vec_09 = insertelement <2 x half> %a_vec_08, half %a_09_conv, i64 1
+    %a_vec_10 = insertelement <2 x half> undef,     half %a_10_conv, i64 0
+    %a_vec_11 = insertelement <2 x half> %a_vec_10, half %a_11_conv, i64 1
+    %a_vec_12 = insertelement <2 x half> undef,     half %a_12_conv, i64 0
+    %a_vec_13 = insertelement <2 x half> %a_vec_12, half %a_13_conv, i64 1
+    %a_vec_14 = insertelement <2 x half> undef,     half %a_14_conv, i64 0
+    %a_vec_15 = insertelement <2 x half> %a_vec_14, half %a_15_conv, i64 1
+
+    %b_vec_00 = insertelement <2 x half> undef,     half %b_00_conv, i64 0
+    %b_vec_01 = insertelement <2 x half> %b_vec_00, half %b_01_conv, i64 1
+    %b_vec_02 = insertelement <2 x half> undef,     half %b_02_conv, i64 0
+    %b_vec_03 = insertelement <2 x half> %b_vec_02, half %b_03_conv, i64 1
+    %b_vec_04 = insertelement <2 x half> undef,     half %b_04_conv, i64 0
+    %b_vec_05 = insertelement <2 x half> %b_vec_04, half %b_05_conv, i64 1
+    %b_vec_06 = insertelement <2 x half> undef,     half %b_06_conv, i64 0
+    %b_vec_07 = insertelement <2 x half> %b_vec_06, half %b_07_conv, i64 1
+    %b_vec_08 = insertelement <2 x half> undef,     half %b_08_conv, i64 0
+    %b_vec_09 = insertelement <2 x half> %b_vec_08, half %b_09_conv, i64 1
+    %b_vec_10 = insertelement <2 x half> undef,     half %b_10_conv, i64 0
+    %b_vec_11 = insertelement <2 x half> %b_vec_10, half %b_11_conv, i64 1
+    %b_vec_12 = insertelement <2 x half> undef,     half %b_12_conv, i64 0
+    %b_vec_13 = insertelement <2 x half> %b_vec_12, half %b_13_conv, i64 1
+    %b_vec_14 = insertelement <2 x half> undef,     half %b_14_conv, i64 0
+    %b_vec_15 = insertelement <2 x half> %b_vec_14, half %b_15_conv, i64 1
+
+    %res = call { float, float, float, float, float, float, float, float } @llvm.nvvm.wmma.mma.sync.col.col.m16n16k16.f32.f32( <2 x half> %a_vec_01, <2 x half> %a_vec_03, <2 x half> %a_vec_05, <2 x half> %a_vec_07, <2 x half> %a_vec_09, <2 x half> %a_vec_11, <2 x half> %a_vec_13, <2 x half> %a_vec_15, <2 x half> %b_vec_01, <2 x half> %b_vec_03, <2 x half> %b_vec_05, <2 x half> %b_vec_07, <2 x half> %b_vec_09, <2 x half> %b_vec_11, <2 x half> %b_vec_13, <2 x half> %b_vec_15, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0, float 0.0e+0)
 
     %res_0 = extractvalue { float, float, float, float, float, float, float, float } %res, 0
     %res_1 = extractvalue { float, float, float, float, float, float, float, float } %res, 1
@@ -153,20 +238,6 @@ wmma_mma(a_0, a_1, a_2, a_3, a_4, a_5, a_6, a_7, b_0, b_1, b_2, b_3, b_4, b_5, b
     ret [8 x float] %ret_7
     "),
     NTuple{8, Float32},
-    Tuple{NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}, NTuple{2, VecElement{Float16}}},
-    convert(NTuple{2, VecElement{Float16}}, a_0),
-    convert(NTuple{2, VecElement{Float16}}, a_1),
-    convert(NTuple{2, VecElement{Float16}}, a_2),
-    convert(NTuple{2, VecElement{Float16}}, a_3),
-    convert(NTuple{2, VecElement{Float16}}, a_4),
-    convert(NTuple{2, VecElement{Float16}}, a_5),
-    convert(NTuple{2, VecElement{Float16}}, a_6),
-    convert(NTuple{2, VecElement{Float16}}, a_7),
-    convert(NTuple{2, VecElement{Float16}}, b_0),
-    convert(NTuple{2, VecElement{Float16}}, b_1),
-    convert(NTuple{2, VecElement{Float16}}, b_2),
-    convert(NTuple{2, VecElement{Float16}}, b_3),
-    convert(NTuple{2, VecElement{Float16}}, b_4),
-    convert(NTuple{2, VecElement{Float16}}, b_5),
-    convert(NTuple{2, VecElement{Float16}}, b_6),
-    convert(NTuple{2, VecElement{Float16}}, b_7))
+    Tuple{NTuple{16, Float16}, NTuple{16, Float16}},
+    convert(NTuple{16, Float16}, a),
+    convert(NTuple{16, Float16}, b))
