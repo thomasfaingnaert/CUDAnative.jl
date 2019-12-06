f4ada4866061f0cdf4ed2ec74c8f65baec0d93df
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-12-06 01:49:13 +0100

    Cleanup

--- src/device/cuda/wmma.jl
+++ src/device/cuda/wmma.jl
@@ -1,5 +1,3 @@
-llvm_wmma_load_a_row_m16n16k16_stride_f16(src_addr, stride) = ccall("extern llvm.nvvm.wmma.m16n16k16.load.a.row.stride.f16.p0i8", llvmcall, NTuple{8, NTuple{2, VecElement{Float16}}}, (Ref{Float16}, Int32), src_addr, stride)
-
 unflatten_recurse(typ::Type{VecElement{T}}, e, idx) where T = :(VecElement{$T}($e[$idx])), idx + 1
 
 function unflatten_recurse(typ::Type{NTuple{N, T}}, e, idx) where {N, T}
