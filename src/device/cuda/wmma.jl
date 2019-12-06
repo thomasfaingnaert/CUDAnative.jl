71d534c9456c87c9599deeec543dc684dab5fdd9
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2019-12-06 01:57:08 +0100

    Cleanup

--- src/device/cuda/wmma.jl
+++ src/device/cuda/wmma.jl
@@ -1,10 +1,8 @@
-unflatten_recurse(typ::Type{VecElement{Float16}}, e, idx) = :(VecElement{Float16}($e[$idx])), idx + 1
-
 function unflatten_recurse(typ::Type{NTuple{N, T}}, e, idx) where {N, T}
     ret = Expr(:tuple)
 
     for (i, eltyp) in enumerate(typ.types)
-        arg, idx = unflatten_recurse(eltyp, e, idx)
+        arg = :(VecElement{Float16}($e[$idx]))
         push!(ret.args, arg)
     end
 
