37e983c65187db61c656fac37b51ab6f25c25f94
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-04-19 00:25:05 +0200

    Add operators

--- src/device/gemm_kernels.jl
+++ src/device/gemm_kernels.jl
@@ -285,4 +285,71 @@ end
 
 end
 
+################################################################################
+# GEMM API: OPERATORS
+################################################################################
+
+module Operator
+
+using CUDAnative: WMMA
+using CUDAnative.Tiling: Tile, linearise
+using CUDAnative.Gemm.Layout: Padded, AlignedColMajor
+
+export op_shape
+export op_fragtype_a, op_fragtype_b, op_fragtype_accum
+export op_load_a, op_load_b, op_load_c, op_store_d
+export op_mma
+
+# ----
+# WMMA
+# ----
+
+export WMMAOp
+struct WMMAOp{M, N, K} end
+
+op_shape(::Type{WMMAOp{M, N, K}}) where {M, N, K} = (M = M, N = N, K = K)
+
+op_fragtype_a(::Type{WMMAOp{16, 16, 16}}, ::Type{Padded{AlignedColMajor{Float16}, PADDING}}) where {PADDING} = WMMA.Fragment{16, 16, 16, 16, Float16, WMMA.ColMajor, WMMA.MatrixA}
+op_fragtype_b(::Type{WMMAOp{16, 16, 16}}, ::Type{Padded{AlignedColMajor{Float16}, PADDING}}) where {PADDING} = WMMA.Fragment{16, 16, 16, 16, Float16, WMMA.ColMajor, WMMA.MatrixB}
+op_fragtype_accum(::Type{WMMAOp{16, 16, 16}}, ::Type{AlignedColMajor{Float32}}) = WMMA.Fragment{16, 16, 16, 8, Float32, WMMA.Unspecified, WMMA.Accumulator}
+
+function op_load_a(::Type{WMMAOp{M, N, K}}, ::Type{Padded{AlignedColMajor{Float16}, PADDING}}, workspace, tile::Tile, logical_size::NamedTuple) where {M, N, K, PADDING}
+    t = Tuple(logical_size)
+    padded_logical_size = typeof(logical_size)((Base.first(t) + PADDING, Base.tail(t)...))
+
+    conf = WMMA.Config{M, N, K, Float32}
+    ptr = pointer(workspace, linearise(tile.index, padded_logical_size))
+    return WMMA.load_a(ptr, padded_logical_size.M, WMMA.ColMajor, conf)
+end
+
+function op_load_b(::Type{WMMAOp{M, N, K}}, ::Type{Padded{AlignedColMajor{Float16}, PADDING}}, workspace, tile::Tile, logical_size::NamedTuple) where {M, N, K, PADDING}
+    t = Tuple(logical_size)
+    padded_logical_size = typeof(logical_size)((Base.first(t) + PADDING, Base.tail(t)...))
+
+    conf = WMMA.Config{M, N, K, Float32}
+    ptr = pointer(workspace, linearise(tile.index, padded_logical_size))
+    return WMMA.load_b(ptr, padded_logical_size.K, WMMA.ColMajor, conf)
+end
+
+function op_load_c(::Type{WMMAOp{M, N, K}}, ::Type{AlignedColMajor{Float32}}, workspace, tile::Tile, logical_size::NamedTuple) where {M, N, K}
+    conf = WMMA.Config{M, N, K, Float32}
+    ptr = pointer(workspace, linearise(tile.index, logical_size))
+    return WMMA.load_c(ptr, logical_size.M, WMMA.ColMajor, conf)
+end
+
+function op_store_d(::Type{WMMAOp{M, N, K}}, ::Type{AlignedColMajor{Float32}}, workspace, frag, tile::Tile, logical_size::NamedTuple) where {M, N, K}
+    conf = WMMA.Config{M, N, K, Float32}
+    ptr = pointer(workspace, linearise(tile.index, logical_size))
+    WMMA.store_d(ptr, frag, logical_size.M, WMMA.ColMajor, conf)
+end
+
+function op_mma(::Type{WMMAOp{M, N, K}}, a_frag, b_frag, c_frag) where {M, N, K}
+    conf = WMMA.Config{M, N, K, Float32}
+    return WMMA.mma(a_frag, b_frag, c_frag, conf)
+end
+
+
+
+end
+
 end
