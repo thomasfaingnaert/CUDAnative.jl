30aaabfb8819c4d6edec11cd522deeeab899ede9
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-04-19 13:12:41 +0200

    Add epilogue

--- src/device/gemm_kernels.jl
+++ src/device/gemm_kernels.jl
@@ -394,6 +394,7 @@ end
 
 end
 
+
 ################################################################################
 # GEMM API: CONFIG & KERNELS
 ################################################################################
@@ -474,6 +475,49 @@ function get_config(; gemm_shape, kwargs...)
         #= Operators =#
         get(params, :operator, WMMAOp{16, 16, 16}),
     }
+end
+
+################################################################################
+# GEMM API: EPILOGUES
+################################################################################
+
+module Epilogue
+
+using CUDAnative: threadIdx, blockIdx
+using CUDAnative.Tiling: Tile, translate, parallellise
+using CUDAnative.Gemm: Config
+using CUDAnative.Gemm.Layout: layout_load, layout_store!
+using GPUifyLoops: @unroll
+
+# ----------------
+# Default epilogue
+# ----------------
+
+export DefaultEpilogue
+
+struct DefaultEpilogue end
+
+@inline function (ep::DefaultEpilogue)(d, shmem_d, transform, conf::Type{Config{GEMM_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR}}) where {GEMM_SHAPE, BLOCK_SHAPE, WARPS_PER_BLOCK, MEM_A_WARP, MEM_A_THREAD, MEM_B_WARP, MEM_B_THREAD, MEM_CD_WARP, MEM_CD_THREAD, COMPUTE_WARP, COMPUTE_OP_SHAPE, GLOBAL_A_LAYOUT, GLOBAL_B_LAYOUT, GLOBAL_C_LAYOUT, GLOBAL_D_LAYOUT, SHARED_A_LAYOUT, SHARED_B_LAYOUT, SHARED_C_LAYOUT, SHARED_D_LAYOUT, OPERATOR}
+    # Constants
+    block_i = (blockIdx().x - 1) * BLOCK_SHAPE.M
+    block_j = (blockIdx().y - 1) * BLOCK_SHAPE.N
+
+    warpId = (threadIdx().x - 1) ÷ 32 + 1
+    laneId = (threadIdx().x - 1) % 32 + 1
+
+    gemm_sz = Tile(GEMM_SHAPE)
+    block_tile = Tile(BLOCK_SHAPE)
+
+    # Cooperatively store a BLOCK_M x BLOCK_N tile of D from shared to global memory within one threadblock
+    @unroll for warp_tile = parallellise(block_tile.MN, MEM_CD_WARP, warpId, WARPS_PER_BLOCK)
+        @unroll for thread_tile = parallellise(warp_tile, MEM_CD_THREAD, laneId, 32)
+            x = layout_load(SHARED_D_LAYOUT, shmem_d, thread_tile, block_tile.MN.size)
+            x = transform(x, thread_tile)
+            layout_store!(GLOBAL_D_LAYOUT, d, x, translate(thread_tile, (M = block_i, N = block_j)), gemm_sz.MN.size)
+        end
+    end
+end
+
 end
 
 end
