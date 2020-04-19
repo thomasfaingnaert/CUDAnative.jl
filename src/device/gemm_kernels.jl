c32d28bd5f2b08c8d2175f070f936a27a0ded615
Thomas Faingnaert <thomas.faingnaert@hotmail.com>
2020-04-19 12:21:48 +0200

    Add transforms

--- src/device/gemm_kernels.jl
+++ src/device/gemm_kernels.jl
@@ -361,4 +361,36 @@ end
 
 end
 
+################################################################################
+# GEMM API: TRANSFORMS
+################################################################################
+
+module Transform
+
+# ---------------------
+# Elementwise transform
+# ---------------------
+
+export ElementwiseTransform
+
+"""
+    ElementwiseTransform{F}
+
+A simple transformation that applies a function elementwise.
+
+# Example
+```julia
+double_elements = ElementwiseTransform(x -> x * 2)
+```
+"""
+struct ElementwiseTransform{F}
+    func::F
+end
+
+@inline ElementwiseTransform() = ElementwiseTransform(identity)
+
+@inline (transf::ElementwiseTransform)(x, tile) = transf.func.(x)
+
+end
+
 end
